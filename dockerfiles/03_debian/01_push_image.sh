a1b9081c1b5d16e6a420b56211831cc6edfe8d43
pepesan <pepesan@gmail.com>
2024-11-11 10:35:55 +0100

    actualizados scripts de uso de para debian

---
+++ dockerfiles/03_debian/01_push_image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# definir el usuario de dockerhub
+DOCKER_HUB_USER=pepesan
+# Definir el nombre de la imagen o repositorio
+DOCKER_HUB_REPOSITORY=ubuntu
+# Antes de hacer el push hay que hacer el login
+# el docker hub
+## pedirá usuario y contraseña
+docker login -u $DOCKER_HUB_USER
+# subir la imagen al Docker hub
+## push es el comando principal
+## tag: usuario/repositorio:tag
+## tag: usuario/nombre_imagen:tag
+docker push $DOCKER_HUB_USER/$DOCKER_HUB_REPOSITORY:latest
+## define otro tag con la fecha actual
+docker push $DOCKER_HUB_USER/$DOCKER_HUB_REPOSITORY:20240716
+## define otro tag con la versión de nuestro software
+docker push $DOCKER_HUB_USER/$DOCKER_HUB_REPOSITORY:24.04
