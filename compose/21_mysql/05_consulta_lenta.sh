b647d41c011894ed5eb4b5d1d17b4375fafd52fe
David Vaquero <pepesan@gmail.com>
2026-03-24 11:02:09 +0100

    mejorado ejemplo de mysql para consultas lentas

---
+++ compose/21_mysql/05_consulta_lenta.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+docker compose exec mysql mysql -u root -proot -e 'SELECT SLEEP(11), "Probando el slow log";'
+
+docker compose exec mysql cat /var/lib/mysql/slow-queries.log
