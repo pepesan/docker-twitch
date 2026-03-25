797b4c94ecbe409b4cc1b449dcaf569eace6b992
David Vaquero <pepesan@gmail.com>
2026-03-25 10:11:26 +0100

    mejorado ejemplo de mysql par replicación

--- compose/22_mysql_replication/02_check_source.sh
+++ compose/22_mysql_replication/02_check_source.sh
@@ -4,4 +4,7 @@ set -e
 
 docker compose exec mysql-source mysql -u root -proot
 
+# entrando desde localhost
+# mysql -h 127.0.0.1 -P 3307 -u root -proot
+
 
--- compose/22_mysql_replication/03_check_source_repl_account.sh
+++ compose/22_mysql_replication/03_check_source_repl_account.sh
@@ -4,4 +4,6 @@ set -e
 
 docker compose exec mysql-source mysql -u repl -preplpass
 
+# entrando desde localhost
+# mysql -h 127.0.0.1 -P 3307 -u repl -preplpass
 
