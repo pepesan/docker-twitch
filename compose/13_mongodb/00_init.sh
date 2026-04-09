d964218d0b0e8830455aa3fe4664863cd9c3c535
David Vaquero <pepesan@gmail.com>
2026-04-09 11:24:08 +0200

    mejorados ejemplos de mongodb

--- compose/13_mongodb/00_init.sh
+++ compose/13_mongodb/00_init.sh
@@ -2,3 +2,5 @@
 
 mkdir -p data
 sudo chmod 777 data
+mkdir -p logs
+sudo chmod 777 logs
--- compose/13_mongodb/30_destroy.sh
+++ compose/13_mongodb/30_destroy.sh
@@ -4,6 +4,7 @@
 docker compose down
 
 sudo rm -rf data
+sudo rm -rf logs
 
 # Volver a config sin auth
 sed -i 's|mongod.conf.auth:/etc/mongod.conf|mongod.conf:/etc/mongod.conf|g' compose.yaml
