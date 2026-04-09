1422955500e56eefce7a3a3d8a08b9a1c0104daf
David Vaquero <pepesan@gmail.com>
2026-04-09 11:03:04 +0200

    metidos ejemplos de mongodb

--- compose/25_garage_s3_compatible/05_create_node.sh
+++ compose/25_garage_s3_compatible/05_create_node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # pilla el ID del comando anterior
-ID=8191e2d23c20bdcf
+ID=5488b1f66d56ecb1
 docker compose exec garage /garage -c /etc/garage.toml layout assign $ID -z dc1 -c 1G
 docker compose exec garage /garage -c /etc/garage.toml layout apply --version 1
 docker compose exec garage /garage -c /etc/garage.toml status
--- compose/25_garage_s3_compatible/10_configure_minio_cli_alias.sh
+++ compose/25_garage_s3_compatible/10_configure_minio_cli_alias.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # pilla estos datos del ./06_create_key.sh
-KEY_ID=GK3eb12766ef42bffaa1e1f635
-SECRET_KEY=e74b6fa840cbd4818ba7d2ff3c3abc6b250fe4f4cb612213872962878adb989b
+KEY_ID=GK2eda03c874ade1b0d5e676cf
+SECRET_KEY=de799bbc1cd7d2aa76308d1f86a1c4cfdda7a38b2c9c35681b7e5e01cec2c4e3
 mc alias set garage http://localhost:3900 $KEY_ID $SECRET_KEY --api S3v4
 
 
